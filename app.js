ee9835f3b599ffc266598dc7364784a53169f9eb
shijinajs <112337210+shijinajs@users.noreply.github.com>
2022-11-29 09:13:31 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const express=require('express')
+const cors=require('cors')
+const logger=require('morgan')
+const jwt = require('jsonwebtoken');
+const PORT=8525
+
+const app=new express()
+
+require('./middlewire/mongoDB')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(logger('dev'))
+
+const path = require('path');
+app.use(express.static(path.join(__dirname + '/dist/frondend')));
+
+
+const api=require('./routers/api')
+app.use('/api',api)
+const url=require('./routers/sign')
+app.use('/api',url)
+
+
+app.get('/',async(req,res)=>{
+res.json('helo')
+})
+app.post('/api/login',async(req,res)=>{
+    try {
+        let email=req.body.email;
+        let password=req.body.password;
+        let payload={
+            'email':email,
+            'password':password,
+            'date':Date.now()
+        }
+        let token=await jwt.sign(payload,'secretCode')//create token
+        res.send({token})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/frondend/index.html'));
+});
+
+
+
+app.listen(PORT,()=>{
+    console.log(`.......server is running at ${PORT}......`)
+})
